1eee676b8adf8e98f1eaafc63e3d97eedc5eb1da
Aimable01 <aimable.kwizera14@gmail.com>
2024-03-04 13:14:02 +0200

    Make the admin authenticate token middleware

---
+++ middleware/adminTokenMiddleware.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { secrets } from "../secrets";
+
+interface RequestExtended extends Request {
+  user?: any;
+}
+
+export const authenticateToken = (
+  req: RequestExtended,
+  res: Response,
+  next: NextFunction
+): void => {
+  const authHead = req.headers["authorization"];
+  const token = authHead && authHead.split(" ")[1];
+
+  if (!token) res.status(404).json({ messsage: "No token found" });
+
+  jwt.verify(token!, secrets.jwt.secretKey, (err, user) => {
+    if (err) {
+      if (err.name === "TokenExpiredError")
+        res.status(400).json({ message: "Token expired. Login again" });
+      res.status(400).json({ message: "An error in verifying token: ", err });
+    }
+    req.user = user;
+    next();
+  });
+};
