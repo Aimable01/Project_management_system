57056afc4647525e8973ae00a96606ada6b5ad15
Aimable01 <aimable.kwizera14@gmail.com>
2024-03-05 11:29:35 +0200

    the main file

--- app.ts
+++ app.ts
@@ -3,6 +3,9 @@ import { pool } from "./database/db";
 import { authenticateToken } from "./middleware/adminTokenMiddleware";
 import { adminAuthRoutes } from "./routes/adminAuthRoutes";
 import { projectRouter } from "./routes/projectRoutes";
+import { taskRouter } from "./routes/taskRoutes";
+import { workerRoutes } from "./routes/workerAuthRoutes";
+import { workerCrudRouter } from "./routes/workersCrudRoutes";
 
 const app = express();
 app.use(express.json());
@@ -10,6 +13,9 @@ app.use(express.json());
 //  routes
 app.use("/admin/auth", adminAuthRoutes);
 app.use("/projects", authenticateToken, projectRouter);
+app.use("/tasks", taskRouter);
+app.use("/worker/auth", workerRoutes);
+app.use("/workerCrud", workerCrudRouter);
 
 //  database connect
 pool
